dc7a739b0b231965c3d9ab1e52218602f5dbe4a1
liu <liu@192.168.0.108>
2024-07-06 22:33:48 +0800

    add usetr auth

--- biz/infrastrature/db/mongodb/connetcion.go
+++ biz/infrastrature/db/mongodb/connetcion.go
@@ -10,7 +10,12 @@ import (
 var MongoClient *mongo.Database
 
 func init() {
-	clientOptions := options.Client().ApplyURI("mongodb://172.30.179.120:27017")
+
+	cre := options.Credential{
+		Username: "root",
+		Password: "e9945f1586f5",
+	}
+	clientOptions := options.Client().ApplyURI("mongodb://172.30.179.120:27017").SetAuth(cre)
 	// 连接到 MongoDB
 	client, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
