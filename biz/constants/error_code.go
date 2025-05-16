767d2628b3072d1fb67bbcefafcecef1f3586a21
l6kv <18602905847@163.com>
2025-05-16 21:03:54 +0800

    appid

--- biz/constants/error_code.go
+++ biz/constants/error_code.go
@@ -2,4 +2,4 @@ package constants
 
 import "errors"
 
-var ERROR_INVALID_USERNAME_OR_PASSWORD = errors.New("invalid username or password")
+var ERROR_INVALID_USERNAME_OR_PASSWORD = errors.New("无效用户或密码，请检查账号，密码以及机构名是否正确")
